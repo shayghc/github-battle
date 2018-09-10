348e9a751b1121bf5fff7253364a8601b7d2e3fb
Seamus Connolly <sghconnolly@gmail.com>
2018-09-10 13:25:23 +0100

    chore: Add queryString library

--- app/components/Results.js
+++ app/components/Results.js
@@ -1,7 +1,11 @@
 var React = require('react');
+var queryString = require('query-string');
 
 class Results extends React.Component {
+
     render() {
+        var players = queryString.parse(this.props.location.search)
+        console.log(players);
         return (
             <div>Results</div>
         )

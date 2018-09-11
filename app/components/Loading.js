7e39bef8b166bb720f14c3ea8e369dfb687eb20e
Seamus Connolly <sghconnolly@gmail.com>
2018-09-11 08:23:13 +0100

    feat: Complete Loading component

--- app/components/Loading.js
+++ app/components/Loading.js
@@ -14,7 +14,30 @@ class Loading extends React.Component {
 
         this.state = {
             text: props.text
-        }
+        };
+    }
+
+    componentDidMount() {
+        var stopper = this.props.text + '...';
+        this.interval = window.setInterval(function() {
+            if(this.state.text === stopper) {
+                this.setState(function() {
+                    return {
+                        test: this.props.text
+                    }
+                })
+            } else {
+                this.setState(function(prevState) {
+                    return {
+                        text: prevState.text + '.'
+                    }
+                });
+            }
+        }.bind(this), this.props.speed);
+    }
+
+    componentWillUnmount() {
+        window.clearInterval(this.interval);
     }
     render() {
         return (
@@ -26,11 +49,13 @@ class Loading extends React.Component {
 }
 
 Loading.propTypes = {
-    text: PropTypes.string.isRequired
+    text: PropTypes.string.isRequired,
+    speed: PropTypes.number.isRequired
 }
 
 Loading.defaultProps = {
-    text: 'Loading'
+    text: 'Loading',
+    speed: 300
 }
 
 module.exports = Loading;

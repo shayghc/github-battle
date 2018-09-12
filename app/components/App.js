0718d4d27a3d11331302e15ff3ac463c5d57c9fb
Seamus Connolly <sghconnolly@gmail.com>
2018-09-12 12:01:00 +0100

    chore: Refactor Results.js for ES6

--- app/components/App.js
+++ app/components/App.js
@@ -5,32 +5,31 @@ var Router = ReactRouter.BrowserRouter;
 var Route = ReactRouter.Route;
 var Switch = ReactRouter.Switch;
 var Nav = require('./Nav');
-'use strict'
 
 var Home = require('./Home');
 var Battle = require('./Battle');
 var Results = require('./Results');
 
 class App extends React.Component {
-  render() {
-    return (
-      <Router>
-        <div className='container'>
-          <Nav />
-          <Switch>
-            <Route exact path='/' component={Home} />
-            <Route exact path='/battle' component={Battle} />
-            <Route path='/battle/results' component={Results} />
-            <Route path='/popular' component={Popular} />
-            <Route render={function() {
-              return <p>Page not found</p>
-            }} />
-          </Switch>
-        </div>
-      </Router>
-      
-    )
-  }
+render() {
+  return (
+    <Router>
+      <div className='container'>
+        <Nav />
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/battle' component={Battle} />
+          <Route path='/battle/results' component={Results} />
+          <Route path='/popular' component={Popular} />
+          <Route render={function() {
+            return <p>Page not found</p>
+          }} />
+        </Switch>
+      </div>
+    </Router>
+    
+  )
+}
 }
 
 module.exports = App;
--- app/components/Results.js
+++ app/components/Results.js
@@ -1,13 +1,12 @@
-var React = require('react');
-var PropTypes = require('prop-types');
-var queryString = require('query-string');
-var api = require('../utils/api');
-var Link = require('react-router-dom').Link;
-var PlayerPreview = require('./PlayerPreview');
-var Loading = require('./Loading');
+const React = require('react');
+const PropTypes = require('prop-types');
+const queryString = require('query-string');
+const api = require('../utils/api');
+const Link = require('react-router-dom').Link;
+const PlayerPreview = require('./PlayerPreview');
+const Loading = require('./Loading');
 
-function Profile(props) {
-    var info = props.info
+function Profile({ info }) {
     return (
         <PlayerPreview avatar={info.avatar_url} username={info.login}>
             <ul className='space-list-items'>
@@ -27,12 +26,12 @@ Profile.propTypes = {
     info: PropTypes.object.isRequired
 }
 
-function Player(props) {
+function Player({ label, score, profile }) {
     return (
         <div>
-            <h1 className='header'>{props.label}</h1>
-            <h3 style={{textAlign: 'center'}}>Score: {props.score}</h3>
-            <Profile info={props.profile}/>
+            <h1 className='header'>{label}</h1>
+            <h3 style={{textAlign: 'center'}}>Score: {score}</h3>
+            <Profile info={profile}/>
         </div>
     )
 }
@@ -55,36 +54,28 @@ class Results extends React.Component {
         }
     }
     componentDidMount() {
-        var players = queryString.parse(this.props.location.search)
+        const { playerOneName, playerTwoName } = queryString.parse(this.props.location.search)
 
         api.battle([
-            players.playerOneName,
-            players.playerTwoName
-        ]).then(function(results) {
-            if(results === null) {
-                return this.setState(function() {
-                    return {
-                        error: 'Something went wrong! Do both entered users exist in GitHub?'
-                    }
-                });
-            }
-
-            this.setState(function() {
-                return {
-                    error: null,
-                    winner: results[0],
-                    loser: results[1],
-                    loading: false
-                }
-            });
-        }.bind(this));
+            playerOneName,
+            playerTwoName
+        ]).then((results) => {
+            results === null
+            ? this.setState(() => ({ 
+                error: 'Something went wrong! Do both entered users exist in GitHub?',
+                loading: false
+            }))
+            : this.setState(() => ({
+                error: null,
+                winner: results[0],
+                loser: results[1],
+                loading: false
+            }))
+        });
     }
 
     render() {
-        var error = this.state.error;
-        var winner = this.state.winner;
-        var loser = this.state.loser;
-        var loading = this.state.loading;
+        const { error, winner, loser, loading } = this.state;
 
         if(loading === true) {
             return <Loading />
